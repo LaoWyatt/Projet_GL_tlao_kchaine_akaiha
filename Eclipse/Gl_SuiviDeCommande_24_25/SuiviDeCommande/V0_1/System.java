8a3a7fc74732a38a27e7261763e3a13c1f44a36d
LaoWyatt <Laowyatt.tcf@gmail.com>
2024-11-24 11:41:44 -1000

    Préparation de ArrayList pour Livreur et Client.

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/System.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/System.java
@@ -1,6 +1,13 @@
 package V0_1;
 
+import java.util.ArrayList;
+
 public class System {
+	
+	ArrayList<?> delivers;
+	ArrayList<?> clients;
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
