0c50504f30266af438c8f119861a9de9e6c719fa
kchaine <160054745+kchaine@users.noreply.github.com>
2024-11-27 13:06:19 -1000

    Réalisation de la classe Livreur

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Livreur.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Livreur.java
@@ -1,33 +1,59 @@
-
 package V0_1;
 
-public class Livreur{
-	static private int _counterLivreur = 0;
-	private int _id_Livreur;
-	public boolean _statut;
+public class Livreur {
 	
-	
-	Livreur(){
-		_id_Livreur = _counterLivreur;
-		_counterLivreur++;
-		_statut= true;
-	}
-	
-	public void setIdLivreur(int id) {
-		_id_Livreur = id;
-	}
-	
-	public int getIdLivreur() {
-		return _id_Livreur;
-	}
-	
-	public void changeStatut() {
-		if (_statut == true) {
-			_statut = false;
-		}else {
-			_statut = true;
-		}
-	}
+	//----------ATTRIBUTS D'INSTANCE----------//
+
+    private int _id_livreur;
+    private String _nom, _prenom;
+    private boolean _disponibilite;
+
+
+    //----------CONSTRUCTEURS----------//
+
+    Livreur(int id_liv, String n, String p, boolean dispo){
+
+        // Vérifie qu'aucune variable est vide
+        boolean verif_vide = !n.equals("") && !p.equals("");
+
+        if (verif_vide){
+            setID(id_liv);
+            setNom(n);
+            setPrenom(p);
+            setDispo(dispo);
+        }
+
+    }
+
+
+    //----------MÉTHODES----------//
+
+    public int getID() {
+        return _id_livreur;
+    }
+    protected void setID(int id){
+        _id_livreur = id;
+    }
+
+    public int getNom() {
+        return _nom;
+    }
+    protected void setNom(int n){
+        _nom = n;
+    }
+    
+    public int getPrenom() {
+        return _prenom;
+    }
+    protected void setPrenom(int p){
+        _prenom = p;
+    }
+    
+    public boolean getDispo() {
+        return _disponibilite;
+    }
+    protected void setDispo(boolean dispo){
+        _disponibilite = dispo;
+    }
 	
 }
-
