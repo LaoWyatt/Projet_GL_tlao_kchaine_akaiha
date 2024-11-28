cbb2ab4faf671ce10ae018d63ff3ffb3d0532904
LaoWyatt <Laowyatt.tcf@gmail.com>
2024-11-27 23:58:27 -1000

    Bis

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/System.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/System.java
@@ -8,6 +8,14 @@ public class System {
 	private ArrayList<Client> _clients;
 	private ListCommande _commandes = new ListCommande();
 	
+	public void addClient() {
+		//;
+	}
+	
+	public void addLivreur() {
+		//;
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
