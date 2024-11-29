06efb33ac23f4f8517d9cfc2ab759275d23c0db7
kchaine <160054745+kchaine@users.noreply.github.com>
2024-11-28 16:42:55 -1000

    Renommage de variables et ajout d'un conteur pour les identifiants

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Commande.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Commande.java
@@ -1,27 +1,27 @@
 package V0_1;
 
+import java.time.LocalDate;
+
 public class Commande {
 	
 	//----------ATTRIBUTS D'INSTANCE----------//
 
     private int _id_commande, _id_client, _id_livreur, _status;
-    private String _date;
+    private LocalDate _date = LocalDate.now();
+    static private int _conteur_com = 1;
 
 
     //----------CONSTRUCTEURS----------//
 
-    Commande(int id_com, int id_cl, int id_liv, int stat, String d){
-
-        // Vérifie qu'aucune variable est vide
-        boolean verif_vide = !d.isEmpty();
-
-        if (verif_vide){
-            setID_com(id_com);
-            setID_cl(id_cl);
-            setID_liv(id_liv);
+    Commande(int id_cl, int id_liv, int stat, LocalDate d){
+            
+    		setID_com(_conteur_com);
+            _id_client = id_cl;
+            _id_livreur = id_liv;
             setStatus(stat);
             setDate(d);
-        }
+            
+            _conteur_com++;
 
     }
 
@@ -31,35 +31,24 @@ public class Commande {
     public int getID_com() {
         return _id_commande;
     }
+    
     protected void setID_com(int id_com){
         _id_commande = id_com;
-    }
-
-    public int getID_cl() {
-        return _id_client;
-    }
-    protected void setID_cl(int id_cl){
-        _id_client = id_cl;
-    }
-    
-    public int getID_liv() {
-        return _id_livreur;
-    }
-    protected void setID_liv(int id_liv){
-        _id_livreur = id_liv;
     }
     
     public int getStatus() {
         return _status;
     }
-    protected void setStatus(int stat) {
+    
+    public void setStatus(int stat) {
         _status = stat;
     }
     
-    public String getDate() {
+    public LocalDate getDate() {
         return _date;
     }
-    protected void setDate(String d){
+    
+    protected void setDate(LocalDate d){
         _date = d;
     }
 	
--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Livreur.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Livreur.java
@@ -7,20 +7,24 @@ public class Livreur {
     private int _id_livreur;
     private String _nom, _prenom;
     private boolean _disponibilite;
+    static private int _conteur_liv = 1;
 
 
     //----------CONSTRUCTEURS----------//
 
-    Livreur(int id_liv, String n, String p, boolean dispo){
+    Livreur(String n, String p, boolean dispo){
 
         // Vérifie qu'aucune variable est vide
         boolean verif_vide = !n.isEmpty() && !p.isEmpty();
 
         if (verif_vide){
-            setID(id_liv);
-            setNom(n);
-            setPrenom(p);
+            setID_liv(_conteur_liv);
+            setNom_liv(n);
+            setPrenom_liv(p);
             setDispo(dispo);
+
+            _conteur_liv++;
+            
         }
 
     }
@@ -28,31 +32,35 @@ public class Livreur {
 
     //----------MÉTHODES----------//
 
-    public int getID() {
+    public int getID_liv() {
         return _id_livreur;
     }
-    protected void setID(int id){
+    
+    protected void setID_liv(int id){
         _id_livreur = id;
     }
 
-    public String getNom() {
+    public String getNom_liv() {
         return _nom;
     }
-    protected void setNom(String n){
+    
+    protected void setNom_liv(String n){
         _nom = n;
     }
     
-    public String getPrenom() {
+    public String getPrenom_liv() {
         return _prenom;
     }
-    protected void setPrenom(String p){
+    
+    protected void setPrenom_liv(String p){
         _prenom = p;
     }
     
     public boolean getDispo() {
         return _disponibilite;
     }
-    protected void setDispo(boolean dispo){
+    
+    public void setDispo(boolean dispo){
         _disponibilite = dispo;
     }
 	
