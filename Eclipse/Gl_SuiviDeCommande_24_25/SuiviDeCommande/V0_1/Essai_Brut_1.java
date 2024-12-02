1e720b252311f8a63d735471f65cc1ee1c0a26bf
LaoWyatt <Laowyatt.tcf@gmail.com>
2024-12-02 13:28:39 -1000

    Moyen de connexion pour client ajouter.

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Essai_Brut_1.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Essai_Brut_1.java
@@ -5,42 +5,70 @@ import java.util.Scanner;
 
 public class Essai_Brut_1 {
 	
-	private ArrayList<Livreur> _delivers;
-	private ArrayList<Client> _clients;
-	private ListCommande _commandes = new ListCommande();
+	private static Compte _connecter = null;
+	private static ArrayList<Livreur> _delivers = new ArrayList<>();
+	private static ArrayList<Client> _clients = new ArrayList<>();
+	private static ListCommande _commandes = new ListCommande();
 	
-	public void addClient() {
+	public static void connexion(int type, String nomUtilisateur, String mdp) {
+		int i = 0;
+		
+		if (type == 0 && !_clients.isEmpty()) {
+			
+			while (i < _clients.size() && _connecter == null) {
+				if (_clients.get(i).get_NomUtilisateur().contentEquals(nomUtilisateur)) {
+					_connecter = _clients.get(i);
+				}
+				i++;
+			}
+			
+		} else if (!_delivers.isEmpty()) {
+			
+			while (i < _delivers.size() && _delivers == null) {
+				if (_delivers.get(i).get_NomUtilisateur().contentEquals(nomUtilisateur)) {
+					_connecter = _delivers.get(i);
+				}
+				i++;
+			}
+			
+		}
+
+		if (_connecter != null && !_connecter.verificationMotDePasse(mdp)) _connecter = null;
+		
+	}
+	
+	public static void addClient() {
 		//;
 	}
 	
-	public void addLivreur() {
+	public static void addLivreur() {
 		//;
 	}
 	
-	public void setDelivers(ArrayList<Livreur> newDelivers){
+	public static void setDelivers(ArrayList<Livreur> newDelivers){
 		_delivers = newDelivers;
 	}
 
 	
 	
-	public ArrayList<Livreur> getDelivers(){
+	public static ArrayList<Livreur> getDelivers(){
 		return _delivers;
 	}
 	
 	
-	public void setClient(ArrayList<Client> newClient){
+	public static void setClient(ArrayList<Client> newClient){
 		_clients = newClient;
 	}
 
 	
 	
-	public ArrayList<Client> getClients(){
+	public static ArrayList<Client> getClients(){
 		return _clients;
 	}
 	
 	
 	
-	public void createCommande(String Contenu, int IdClient, int IdLivreur) {
+	public static void createCommande(String Contenu, int IdClient, int IdLivreur) {
 		//Commande newCommande =  new Commande();
 	}
 	
@@ -49,15 +77,17 @@ public class Essai_Brut_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		int decision = -1;
+		_clients.add(new Client("Hello","World","1234","python","password"));
+		
 		
 		while (decision != 0){
 			System.out.print("----{ Suivie de commande 1 }----\n\n");
 			
 			System.out.print("[ Liste de processus ]\n>> Saisisez 0 pour quitter\n\n");
-			System.out.print("1> ...\n");
-			System.out.print("2> ...\n");
+			System.out.print("1> Connexion\n");
+			System.out.print("2> Inscription\n");
 			System.out.print("3> ...\n");
 			System.out.print("4> ...\n");
 			System.out.print("5> ...\n");
@@ -74,7 +104,29 @@ public class Essai_Brut_1 {
 				break;
 				
 			case 1:
-				System.out.print("Hello Process !\n\n");
+				System.out.print(">>> Connexion du compte <<<\n\n");
+				System.out.print("Nom utilisateur : ");
+				
+				String nomUtilisateur = keyboard.next();
+				
+				System.out.print("\nMot de passe : ");
+				
+				String mdp  = keyboard.next();
+				
+				System.out.print("\nLivreur (Y/N) :");
+				
+				String type = keyboard.next();
+				int logicType = 0;
+				if (type == "Y") logicType = 1;
+				
+				connexion(logicType,nomUtilisateur,mdp);
+				
+				if (_connecter == null) {
+					System.out.print("\n\nLogin ou Mot de passe incorrect !\n\n");
+				} else {
+					System.out.print("\n\nBienvunue "+ _connecter.get_NomUtilisateur() +"\n\n");
+				}
+				
 				break;
 				
 			default:
