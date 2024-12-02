7535a27cc80f26f597c84954649838495fd93107
LaoWyatt <Laowyatt.tcf@gmail.com>
2024-12-02 13:27:22 -1000

    Adaption des versions et correction du constructeur et de 'verificationMotDePasse'
    Variable _graine attribuée au début du constructeur.
    
    Correction de la vérification == par contentEquals

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Compte.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Compte.java
@@ -3,14 +3,13 @@ package V0_1;
 public class Compte{
 	private String _nomUtilisateur;
 	private int _graine;
-	private String _motDePasse;
+	public String _motDePasse;
 	
 	
 	Compte(String nouvNU, String nouvMDP){
-		
-		set_NomUtilisateur(nouvNU);
-		setMotDePasse(nouvMDP);
+	set_NomUtilisateur(nouvNU);
 		_graine = (int)(Math.random() * 101);
+		setMotDePasse(nouvMDP);
 	
 	}
 	
@@ -29,22 +28,20 @@ public class Compte{
 			_motDePasse = chiffrement(nouvMDP);
 			return true;
 		}
-		
 		return false;
 	}
 	
-	private String chiffrement(String mdp) {
+	public String chiffrement(String mdp) {
 		String chif = "";
 		
 		for (int i = 0; i < mdp.length(); i++) {
 			chif += (char) (((int)mdp.charAt(i) * _graine + 8 - 10 + 6 * _graine) % 255);
 		}
-		
 		return chif;
 	}
 	
 	protected boolean verificationMotDePasse(String mdp) {
-		return _motDePasse == chiffrement(mdp);
+		return _motDePasse.contentEquals(chiffrement(mdp));
 	}
 }
 
