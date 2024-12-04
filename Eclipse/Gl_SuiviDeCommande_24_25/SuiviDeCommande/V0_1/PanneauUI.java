49906f7b8e0d50d5fd50f6d9dcbb755a124bf345
LaoWyatt <Laowyatt.tcf@gmail.com>
2024-12-03 22:32:22 -1000

    Creation classe 'PanneauUI

---
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/PanneauUI.java
@@ -0,0 +1,29 @@
+package V0_1;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+abstract public class PanneauUI extends JPanel {
+
+	GestionUI _fenetre;
+	protected JPanel _entete;
+	public JPanel _corps;
+	protected JLabel _titre;
+	
+	PanneauUI(GestionUI fenetre){
+		super();
+		_fenetre = fenetre;
+		_entete = new JPanel();
+		_corps = new JPanel();
+		_titre = new JLabel();
+		this.setLayout(new BorderLayout());
+	    _titre.setFont(new Font("SansSerif",Font.BOLD,18));
+	    _entete.add(_titre);
+		this.add(_entete,BorderLayout.NORTH);
+	}
+	
+	abstract void update();
+	
+}
