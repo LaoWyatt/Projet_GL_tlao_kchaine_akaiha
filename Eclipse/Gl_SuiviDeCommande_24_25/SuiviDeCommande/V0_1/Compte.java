76fb57d1433ac3b56743fdd2d83f3c8bda2c1520
kchaine <160054745+kchaine@users.noreply.github.com>
2024-12-05 09:29:40 -1000

    Ajout d'un nom et prénom à la classe compte

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Compte.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/Compte.java
@@ -1,38 +1,49 @@
 package V0_1;
 
-public class Compte{
+public class Compte {
+	private String _nom;
+	private String _prenom;
 	private String _nomUtilisateur;
 	private int _graine, _type;
 	private String _motDePasse;
 	
-	
-	Compte(String nouvNU, String nouvMDP, int type){
-	set_NomUtilisateur(nouvNU);
+	// type d'un compte utilisateur = Client, Livreur ou Administrateur.
+	Compte(String nouvNom, String nouvPrenom, String nouvNU, String nouvMDP, int type){
+		
+		set_NomCompte(nouvNom);
+		set_PrenomCompte(nouvPrenom);
+		set_NomUtilisateur(nouvNU);
 		_graine = (int)(Math.random() * 101);
 		setMotDePasse(nouvMDP);
 		_type = type;
 	}
 	
+	public String get_NomCompte() {
+		return _nom;
+	}
+	
+	protected void set_NomCompte(String nouvNU) {
+		_nom = nouvNU;
+	}
+	
+	public String get_PrenomCompte() {
+		return _prenom;
+	}
+	
+	protected void set_PrenomCompte(String nouvNU) {
+		_prenom = nouvNU;
+	}
 	
 	public String get_NomUtilisateur() {
 		return _nomUtilisateur;
-	}
-	
-	public int get_Type() {
-		return _type;
 	}
 	
 	protected void set_NomUtilisateur(String nouvNU) {
 		_nomUtilisateur = nouvNU;
 	}
 	
-	private boolean setMotDePasse(String nouvMDP) {
-		
-		if (!nouvMDP.isEmpty()) {
-			_motDePasse = chiffrement(nouvMDP);
-			return true;
-		}
-		return false;
+	public int get_Type() {
+		return _type;
 	}
 	
 	private String chiffrement(String mdp) {
@@ -42,6 +53,15 @@ public class Compte{
 			chif += (char) (((int)mdp.charAt(i) * _graine + 8 - 10 + 6 * _graine) % 255);
 		}
 		return chif;
+	}
+	
+	protected boolean setMotDePasse(String nouvMDP) {
+		
+		if (!nouvMDP.isEmpty()) {
+			_motDePasse = chiffrement(nouvMDP);
+			return true;
+		}
+		return false;
 	}
 	
 	protected boolean verificationMotDePasse(String mdp) {
