6a5e0e24d2f2c5662c37b5d140f8a63887010c4f
ak <ariivai.kaiha@etudiant.upf.pf>
2024-12-04 15:09:26 -1000

    Continuation test unitaire

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/test_unitaire.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/test_unitaire.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.BeforeAll;
 
 
 public class test_unitaire{
-	public Compte testCompte;
-	public Administrateur testAdmin;
-	public Livreur testLivreur;
-	public Commande testCommande;
-	public ListCommande testListCommande;
+	public Compte _testCompte;
+	public Client _testClient;
+	public Administrateur _testAdmin;
+	public Livreur _testLivreur;
+	public Commande _testCommande;
+	public ListCommande _testListCommande;
 
 	
 	
@@ -17,19 +18,38 @@ public class test_unitaire{
 	public void testCreerCompte() {
 		 String nomTest = "Test";
 		 String mdpTest = "123";
-		 Compte compteTest = new Compte(nomTest,mdpTest,1);
+		 Compte compteTestLocal = new Compte(nomTest,mdpTest,1);
 		 
-		assertEquals(compteTest.get_NomUtilisateur, nomTest);
+		assertEquals(compteTestLocal.get_NomUtilisateur(), nomTest);
 		/**Faire pour le mdp**/
+		_testCompte = compteTestLocal;
+		
 		
 	}
 
+@BeforeAll
+	public void testCreerClient() {
+	String nomTest = "TestN";
+	String prenomTest = "TestP";
+	String cpTest = "TestCp";
+	String adresseTest = "TestA";
+	String mdpTest = "TestMdp";
+	
+	Client clientTestLocal = new Client(nomTest,prenomTest,cpTest,adresseTest,mdpTest);
+	assertEquals(clientTestLocal.getNom_Client(),nomTest);
+	assertEquals(clientTestLocal.getPrenom_Client(),prenomTest);
+	assertEquals(clientTestLocal.getCodePostal(),cpTest);
+	assertEquals(clientTestLocal.getNom_Client(),nomTest);
+	
+	
+}
 @BeforeAll
 	public void testCreerCommande() {
 	int idClientTest = 1;
 	Commande commandeTest = new Commande(idClientTest);
 	
 	assertEquals(commandeTest.getID_Client(), idClientTest);
+	_testCommande = commandeTest;
 
 }
 
