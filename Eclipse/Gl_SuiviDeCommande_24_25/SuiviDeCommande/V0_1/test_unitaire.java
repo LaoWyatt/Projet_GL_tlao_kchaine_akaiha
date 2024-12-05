df80b85f577ea3d0caf9767aa7920aa3937911ab
ak <ariivai.kaiha@etudiant.upf.pf>
2024-12-05 08:32:38 -1000

    .

--- Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/test_unitaire.java
+++ Eclipse/Gl_SuiviDeCommande_24_25/SuiviDeCommande/V0_1/test_unitaire.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class test_unitaire{
-	public Compte _testCompte;
-	public Client _testClient;
-	public Administrateur _testAdmin;
-	public Livreur _testLivreur;
-	public Commande _testCommande;
-	public ListCommande _testListCommande;
+	public static Compte _testCompte;
+	public static Client _testClient;
+	public static Administrateur _testAdmin;
+	public static Livreur _testLivreur;
+	public static Commande _testCommande;
+	public static ListCommande _testListCommande;
 
 	
 	
 @BeforeAll
-	public void testCreerCompte() {
+	public static void testCreerCompte() {
 		 String nomTest = "Test";
 		 String mdpTest = "123";
 		 Compte compteTestLocal = new Compte(nomTest,mdpTest,1);
@@ -31,7 +32,7 @@ public class test_unitaire{
 	}
 
 @BeforeAll
-	public void testCreerClient() {
+	public  static void testCreerClient() {
 	String nomTest = "TestN";
 	String prenomTest = "TestP";
 	String cpTest = "TestCp";
@@ -51,7 +52,7 @@ public class test_unitaire{
 	
 }
 @BeforeAll
-	public void testCreerCommande() {
+	public static void testCreerCommande() {
 	int idClientTest = 1;
 	Commande commandeTestLocal = new Commande(idClientTest);
 	
@@ -60,22 +61,34 @@ public class test_unitaire{
 
 }
 
+@BeforeAll
+	public static void testCreerListCommandes() {
+	ListCommande testListCommandeLocal = new ListCommande();
+	_testListCommande = testListCommandeLocal;
+	
+}
+
 
 
 @Test
-	public void testGetListCommande() {
-	ListCommande testListCommandeLocal = new ListCommande();
-	testListCommandeLocal.ajoutCommande(_testCommande);
+	public void testAddListCommande() {
+	_testListCommande.ajoutCommande(_testCommande);
 	ArrayList<Commande> testList = new ArrayList<Commande>();
-	testList = testListCommandeLocal.getCommandes();
+	testList = _testListCommande.getCommandes();
 	
 	assertEquals(testList.size(), 1);
 	assertEquals(testList.get(0).getID_Commande(), _testCommande.getID_Commande());
 	
+}	
 	
-	
-	
-}
+@Test
+
+	public void testSupListCommande() {
+		_testListCommande.supCommande(_testCommande.getID_Client());
+		ArrayList<Commande> testList = new ArrayList<Commande>();
+		testList = _testListCommande.getCommandes();
+		assertFalse(testList.contains(_testCommande));
+	}
 
 
 
